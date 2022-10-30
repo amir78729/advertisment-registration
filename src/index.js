72242a7bae7ee4a34f00ed4e6e96104cdbc0b8b0
Amirhossein Alibakhshi <amir.alibakhshi@tapsi.cab>
2022-10-31 01:07:52 +0330

    trying to upload file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
+const path = require('path');
 const morgan = require('morgan');
 const sendError = require('./utils/errorHandler');
 const sendResponse = require('./utils/responseHandler');
@@ -11,6 +12,8 @@ const credentials = require('./credentials');
 const AdvertisementDTO = require('./DTO/Advertisement');
 const { sendMail } = require('./services/mailgun');
 const { processImage } = require('./services/imagga');
+const multer = require('multer');
+// const upload = multer();
 
 mongoose.connect(credentials?.mongoUri, { useUnifiedTopology: true, useNewUrlParser: true });
 
@@ -23,8 +26,84 @@ connection.once('open', function() {
 const app = express();
 app.use(helmet());
 app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(cors());
 app.use(morgan('combined'));
+
+// for parsing multipart/form-data
+// app.use(upload.array());
+// app.use(express.static('public'));
+const PATH = './src/';
+
+/**
+ * Insert new file.
+ * @return{obj} file Id
+ * */
+async function uploadBasic() {
+    const fs = require('fs');
+    const {GoogleAuth} = require('google-auth-library');
+    const {google} = require('googleapis');
+    
+    // Get credentials and build service
+    // TODO (developer) - Use appropriate auth mechanism for your app
+    const auth = new GoogleAuth({
+        scopes: 'https://www.googleapis.com/auth/drive',
+    });
+    const service = google.drive({version: 'v3', auth});
+    const fileMetadata = {
+        name: 'photo.jpg',
+    };
+    const media = {
+        mimeType: 'image/jpeg',
+        body: fs.createReadStream('./src/138728.jpg'),
+    };
+    try {
+        const file = await service.files.create({
+            resource: fileMetadata,
+            media: media,
+            fields: 'id',
+        });
+        console.log('File Id:', file.data.id);
+        return file.data.id;
+    } catch (err) {
+        // TODO(developer) - Handle error
+        throw err;
+    }
+}
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, PATH);
+    },
+    filename: (req, file, cb) => {
+        const fileName = file.originalname.toLowerCase().split(' ').join('-');
+        console.log('file', file)
+        cb(null, fileName)
+    }
+});
+
+const upload = multer({
+    storage: storage,
+    fileFilter: (req, file, cb) => {
+        if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg" || file.mimetype == "image/gif") {
+            cb(null, true);
+        } else {
+            cb(null, false);
+            return cb(new Error('Allowed only .png, .jpg, .jpeg and .gif'));
+        }
+    }
+});
+
+// app.post('/add', (req, res, next) => {
+app.post('/add', upload.single('image'), async (req, res, next) => {
+    // console.log({
+    //     name: req.body.name,
+    //     imageURL: req.file.path
+    // })
+    await uploadBasic();
+    sendResponse(res, [])
+})
 
 app.get('/', (req, res) => {
     res.send([]);
@@ -51,6 +130,7 @@ app.get('/ad/:id', async (req, res) => {
     try {
         const { id } = req.params;
         const data = await Advertisement.findOne({ id });
+        // res.send("recieved your request!");
         sendResponse(res, AdvertisementDTO.output(data));
     } catch (error) {
         sendError(res, error);
@@ -69,11 +149,14 @@ app.delete('/ad/:id', async (req, res) => {
 
 app.post('/ad', async (req, res) => {
     try {
-        const { image, description, email } = req.body;
-        const data = new Advertisement({ image, description, email, state: 'PENDING', category: 'UNKNOWN' });
-        await data.save();
+        console.log(req.body);
+        // const { image, description, email } = req.body;
+        // const data = new Advertisement({ image, description, email, state: 'PENDING', category: 'UNKNOWN' });
+        // await data.save();
+        
         // sendMail(email, 'ثبت آگهی', `آگهی شما با شناسه‌ی ${data?.id || '?'} ثبت گردید. وضعیت این آگهی پس از مدتی برای شما ارسال خواهد شد.`)
-        sendResponse(res, AdvertisementDTO.output(data), `آگهی شما با شناسه‌ی ${data?.id || '?'} ثبت گردید.`);
+        // sendResponse(res, AdvertisementDTO.output(data), `آگهی شما با شناسه‌ی ${data?.id || '?'} ثبت گردید.`);
+        sendResponse(res, {});
     } catch (error) {
         sendError(res, error);
     }
